8cdf3e856a14a258104fb4eeddd597c648a10f14
SaivikasThota <146901656+SaivikasThota@users.noreply.github.com>
2023-10-15 00:45:57 -0700

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home - Our Mobile Store</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Create a CSS file for styling -->
+</head>
+<body>
+    <header>
+        <h1>Welcome to Mobile Devices Inc.</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="hero">
+        <h2>Your Mobile Journey Begins Here</h2>
+        <p>Discover the latest mobile devices, accessories, and expert services at Mobile Devices Inc.</p>
+        <a href="services.php" class="btn">Explore</a>
+    </section>
+
+    <h2 style="text-align:center">Featured Products</h2>
+    <section class="featured-products">
+        
+        <div class="product">
+            <img src="product1.jpg" alt="Product 1">
+            <h3>IPhone 15 pro max</h3>
+            <p>
+                Experience the cutting-edge technology with our latest smartphone. A perfect blend of style and performance.
+            </p>
+            <p class="price">$799</p>
+        </div>
+        <div class="product">
+            <img src="product2.jpg" alt="Product 2">
+            <h3>Wireless Earbuds</h3>
+            <p>
+                Enjoy superior sound quality and convenience with our wireless earbuds. Perfect for music and calls on the go.
+            </p>
+            <p class="price">$129</p>
+        </div>
+    </section>
+
+    <footer>
+        &copy; 2023 Mobile Devices Inc. All rights reserved.
+    </footer>
+</body>
+</html>
---
+++ api/services.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Our Services - Mobile Store</title>
+    <link rel="stylesheet" type="text/css" href="styles.css"> <!-- Link to your CSS file -->
+</head>
+<body>
+    <header>
+        <h1>Our Services</h1>
+    </header>
+    
+    <nav>
+        <ul>
+            <li><a href="index.php">Home</a></li>
+            <li><a href="about.php">About</a></li>
+            <li><a href="services.php">Services</a></li>
+            <li><a href="contact.php">Contact</a></li>
+        </ul>
+    </nav>
+
+    <section class="services">
+        <h2>What We Offer</h2>
+        <ul>
+            <li>Wide Selection of Mobile Devices</li>
+            <li>Accessories and Gadgets</li>
+            <li>Device Repairs and Maintenance</li>
+            <li>Technical Support and Consultation</li>
+        </ul>
+    </section>
+
+    <!-- Additional content for the services page goes here -->
+
+    <footer>
+    &copy; 2023 Mobile Devices Inc. All rights reserved.
+    </footer>
+</body>
+</html>
