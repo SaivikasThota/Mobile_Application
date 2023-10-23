362d0cf4e34792523fd624d6c56c2106bc0d2193
sonu <sonuvicky9045@gmail.com>
2023-10-23 02:01:58 -0700

    Added Secure section

--- api/about.php
+++ api/about.php
@@ -15,6 +15,7 @@
             <li><a href="about.php">About</a></li>
             <li><a href="services.php">Services</a></li>
             <li><a href="contact.php">Contact</a></li>
+            <li><a href="secure.php">Contact</a></li>
         </ul>
     </nav>
 
